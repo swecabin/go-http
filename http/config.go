8d6415d3b7a77b01f7330287d6f67b255c3e4997
Aravindhan <aravindhan.a@lynk.co.in>
2019-04-25 12:35:42 +0530

    📝 Updated comments for godoc

--- http/config.go
+++ http/config.go
@@ -1,5 +1,6 @@
 package http
 
+// Config is used to specify configuration for httpwrapper
 type Config struct {
 	// HTTP timeout in seconds. Default - 10 second
 	timeout int
@@ -10,7 +11,7 @@ type Config struct {
 	retryFactor float64
 }
 
-// Creates and initialises config to default values
+// NewConfig Creates and initialises config to default values
 func NewConfig() *Config {
 	return &Config{
 		timeout:     10,
@@ -20,23 +21,23 @@ func NewConfig() *Config {
 	}
 }
 
-// Function to add new HTTP header for all requests. k - key of header (Authorisation etc.)
+// AddHeader is used to add new HTTP header for all requests. k - key of header (Authorisation etc.)
 // v - Value of the header
 func (c *Config) AddHeader(k, v string) {
 	c.headers[k] = v
 }
 
-// Function to Set timeout for each HTTP requests
+// SetTimeout is used to Set timeout for each HTTP requests
 func (c *Config) SetTimeout(timeout int) {
 	c.timeout = timeout
 }
 
-// Function to set number of reties
+// SetRetries is used to set number of reties
 func (c *Config) SetRetries(retries int) {
 	c.retries = retries
 }
 
-// Function to set retry factor for exponential backoff
+// SetRetryFactor is used to set retry factor for exponential backoff
 func (c *Config) SetRetryFactor(factor float64) {
 	c.retryFactor = factor
 }
--- http/http-wrapper.go
+++ http/http-wrapper.go
@@ -4,10 +4,12 @@ import (
 	"github.com/FenixAra/go-util/log"
 )
 
+// HttpWrapper is the abstracted interface of http
 type HttpWrapper interface {
 	MakeRequest(method, url, name string, req, res interface{}) error
 }
 
+// New is used to get new HTTP wrapper object
 func New(config *Config, l *log.Logger) HttpWrapper {
 	return &httpwrapper{
 		c: config,
